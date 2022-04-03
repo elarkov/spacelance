50565b0bb2a0deab52671e84884f52f4b7430051
Evgeniy <elarkov@inbox.ru>
2022-04-03 14:37:15 +0700

    Remove scripts

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,12 @@
 
 import * as functions from "./modules/functions.js";
-import * as phoneinput from "./modules/phoneinput.js";
 import * as slider from "./modules/slider.js";
-import * as micromodal from "./modules/micromodal.js";
-import * as lazyload from "./modules/lazyload.js";
 import * as sideMenu from "./modules/side-menu.js";
 import * as validate from "./modules/validata.js";
 
 
 functions.isWebp();
-phoneinput.phoneInput();
-micromodal.renderPopup();
 sideMenu.sideMenu();
-lazyload.lazyLoadImg();
 validate.validateField();
 slider.renderSlider();
 
